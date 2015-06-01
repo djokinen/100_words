1b19dd75a9778ffd82bb5deeec49e6cdf4b6d971
djokinen <djokinen@yahoo.com>
2015-06-01 15:53:49 -0400

    test
    test

---
+++ www/hello.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="hello.aspx.cs" Inherits="www_hello" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+</head>
+<body>
+	<form id="form1" runat="server">
+		<h1>Hello World</h1>
+		<p id="lorem" runat="server"></p>
+	</form>
+</body>
+</html>
---
+++ www/hello.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class www_hello : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		lorem.InnerText = ClientQueryString; // "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+	}
+}
